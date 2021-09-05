5878e2d4aa96a92b377e46b746bb8c480d806d41
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-04 21:37:20 -0400

    Update piece.py

--- checkers/checker/piece.py
+++ checkers/checker/piece.py
@@ -1,8 +1,22 @@
-class Piece():
-    def __init__(self, x, y, color):
-        self.x = x
+import pygame
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, x, y, color, dir, size=30):
+        super().__init__()
+        self.image = pygame.Surface((80,80), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, (color), (40,40), size)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x*90 +45
+        self.rect.centery = y*90 +45
+        self.x = x 
         self.y = y
         self.color = color
-        self.size = 25
         self.king = False
         self.valid_moves = []
+        self.dir = dir #row direction
+
+class Moves(Piece):
+    def __init__(self, x, y):
+        super().__init__(x, y, (0, 0, 255), 0) 
+    
+        
+
