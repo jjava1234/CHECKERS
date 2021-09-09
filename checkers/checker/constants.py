8cf8cdc0a06c7123d5c5202025eb93e87f7f506b
jjava1234 <81831375+jjava1234@users.noreply.github.com>
2021-09-08 21:49:25 -0400

    Create constants.py

---
+++ checkers/checker/constants.py
@@ -0,0 +1,5 @@
+import pygame
+
+win = pygame.display.set_mode((720,720))
+colors = {"red":(255,0,0), "black":(0,0,0)} 
+crown = pygame.transform.scale(pygame.image.load('checker/asset/crown.png'), (44, 25))
